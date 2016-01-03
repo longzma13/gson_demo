4dd5a20d2f406806b38089097dd521591d38ff31
Le Kim Long <le.long@tt-tech.de>
2016-01-03 20:41:41 +0700

    update

--- GsonUlti/gsonulti/src/main/java/vn/eazy/gsonulti/IActionGson.java
+++
@@ -1,9 +0,0 @@
-package vn.eazy.gsonulti;
-
-/**
- * Created by longzma11 on 1/2/16.
- */
-public interface IActionGson {
-    void writeObjectToJson(Object o);
-    <T> T getObjectfromJson(String json,Class<T> _class);
-}
